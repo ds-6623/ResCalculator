d00f52813899e04733e249e5614cde0a5d667f2a
Nikita Rabotnov <76934344+ds-6623@users.noreply.github.com>
2022-08-07 12:58:58 -0400

    v1.2.0

--- rescalc.js
+++ rescalc.js
@@ -1,13 +1,13 @@
 function calc() {
-    let a = document.getElementById('OverallInput').value;
-    let b = document.getElementById('AtmInput').value;
-    let c = ( a - b ) / 2;
-    let d = document.getElementById('SpeedInput').value;
-    let e = c / d;
-    
-    document.getElementById('result1').innerHTML = c;
-    console.log(c)
+    let a = document.getElementById('overall').value;
+    let b = document.getElementById('inventory').value;
+    let c = document.getElementById('mining').value;
+    let d = a - b;
+    let e = ( a - b ) / 2;
+    let f = ( ( a - b ) / 2 ) / c;
 
+    document.getElementById('result1').innerHTML = d;
     document.getElementById('result2').innerHTML = e;
-    console.log(e)
+    document.getElementById('result3').innerHTML = f;
+
 }
